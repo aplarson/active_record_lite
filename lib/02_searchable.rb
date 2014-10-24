bad49b02db5f405bbe6601cbb9c0d8268e9d58c1
App Academy Student <student@appacademy.io>
2014-10-24 13:27:47 -0700

    Add simple where queries

--- lib/02_searchable.rb
+++ lib/02_searchable.rb
@@ -3,10 +3,20 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+    where_clause = params.keys.map { |column| "#{column} = ?"}.join(' AND ')
+    values_to_find = params.values.map { |val| val.to_s }
+    results = DBConnection.execute(<<-SQL, *values_to_find)
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+      WHERE
+        #{where_clause} 
+    SQL
+    parse_all(results)
   end
 end
 
 class SQLObject
-  # Mixin Searchable here...
+  extend Searchable
 end
